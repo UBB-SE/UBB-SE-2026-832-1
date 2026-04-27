029e50d7940487c426cb58ce5d935dde2279a1bf
Mario Maxim <maximmarioandrei@gmail.com>
2026-04-27 12:21:50 +0300

    refactor WebAPI to utilize dependency injection for data access

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,8 +1,4 @@
-using ClassLibrary.Data;
-using ClassLibrary.IRepositories;
-using ClassLibrary.Repositories;
-using ClassLibrary.Models;
-using Microsoft.EntityFrameworkCore;
+using ClassLibrary.DependencyInjection;
 using WebAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,29 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseInMemoryDatabase("AppDb"));
-builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddClassLibraryDataAccess();
 builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-
-    if (!dbContext.Users.Any())
-    {
-        dbContext.Users.Add(
-            new User
-            {
-                Id = Guid.NewGuid(),
-                Email = "test@example.com",
-                FullName = "test user"
-            });
-        dbContext.SaveChanges();
-    }
-}
+app.Services.SeedClassLibraryData();
 
 if (app.Environment.IsDevelopment())
 {
