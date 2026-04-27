e804fcede38832edad2223874e7857f7461a7416
Mario Maxim <maximmarioandrei@gmail.com>
2026-04-27 11:59:02 +0300

    Add Entity Framework Core with AppDbContext

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using ClassLibrary.Data;
 using ClassLibrary.IRepositories;
-using WebAPI.Repositories;
+using ClassLibrary.Repositories;
+using ClassLibrary.Models;
+using Microsoft.EntityFrameworkCore;
 using WebAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,10 +10,29 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("AppDb"));
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    if (!dbContext.Users.Any())
+    {
+        dbContext.Users.Add(
+            new User
+            {
+                Id = Guid.NewGuid(),
+                Email = "test@example.com",
+                FullName = "test user"
+            });
+        dbContext.SaveChanges();
+    }
+}
 
 if (app.Environment.IsDevelopment())
 {
