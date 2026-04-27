77003151e061e62abb95c0e7233dbc176fe37b43
AndaChiorean <andachiorean@users.noreply.github.com>
2026-04-27 19:24:00 +0300

    register nut entities in AppDbContext and add seed data

--- ClassLibrary/Data/AppDbContext.cs
+++ ClassLibrary/Data/AppDbContext.cs
@@ -6,5 +6,33 @@ namespace ClassLibrary.Data;
 public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; } = default!;
+
+    public DbSet<NutUser> NutUsers { get; set; } = default!;
+
+    public DbSet<UserData> UserData { get; set; } = default!;
+
+    public DbSet<Meal> Meals { get; set; } = default!;
+
+    public DbSet<MealPlan> MealPlans { get; set; } = default!;
+
+    public DbSet<Ingredient> Ingredients { get; set; } = default!;
+
+    public DbSet<Inventory> Inventories { get; set; } = default!;
+
+    public DbSet<ShoppingItem> ShoppingItems { get; set; } = default!;
+
+    public DbSet<Reminder> Reminders { get; set; } = default!;
+
+    public DbSet<DailyLog> DailyLogs { get; set; } = default!;
+
+    public DbSet<Conversation> Conversations { get; set; } = default!;
+
+    public DbSet<Message> Messages { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Ingredient>()
+            .HasKey(i => i.FoodId);
+    }
 }
 
