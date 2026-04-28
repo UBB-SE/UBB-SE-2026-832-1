b0777d619d403318d001f92bf5298fa43549ecfb
Alawerga <adrianfdez.rdez@gmail.com>
2026-04-28 15:22:36 +0300

    migrate Ingredient model and repository to EF Core. #51

---
+++ ClassLibrary/Models/Ingredient.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClassLibrary.Models;
+
+/// <summary>
+/// Represents a food ingredient with nutritional values per 100g.
+/// </summary>
+public class Ingredient
+{
+    [Key]
+    public int FoodId { get; set; }
+
+    [Required]
+    [MaxLength(255)]
+    public string Name { get; set; } = string.Empty;
+
+    public double CaloriesPer100g { get; set; }
+
+    public double ProteinPer100g { get; set; }
+
+    public double CarbohydratesPer100g { get; set; }
+
+    public double FatPer100g { get; set; }
+}
---
+++ ClassLibrary/Repositories/IngredientRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLibrary.Data;
+using ClassLibrary.IRepositories;
+using ClassLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClassLibrary.Repositories;
+
+/// <summary>
+/// Entity Framework Core implementation of the Ingredient repository.
+/// </summary>
+public class IngredientRepository : IIngredientRepository
+{
+    private readonly AppDbContext _context;
+
+    public IngredientRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> GetOrCreateIngredientIdAsync(string name)
+    {
+        return await GetOrCreateIngredientIdByNameAsync(name);
+    }
+
+    public async Task<int> GetOrCreateIngredientIdByNameAsync(string name)
+    {
+       
+        var ingredient = await _context.Ingredients
+            .FirstOrDefaultAsync(i => i.Name.ToLower() == name.ToLower());
+
+        if (ingredient != null)
+        {
+            return ingredient.FoodId;
+        }
+
+
+        var newIngredient = new Ingredient
+        {
+            Name = name,
+            CaloriesPer100g = 0,
+            ProteinPer100g = 0,
+            CarbohydratesPer100g = 0,
+            FatPer100g = 0
+        };
+
+        await _context.Ingredients.AddAsync(newIngredient);
+        await _context.SaveChangesAsync();
+
+        return newIngredient.FoodId;
+    }
+
+    public async Task<List<KeyValuePair<int, string>>> SearchIngredientsAsync(string search)
+    {
+        return await _context.Ingredients
+            .Where(i => EF.Functions.Like(i.Name, $"%{search}%"))
+            .OrderBy(i => i.Name)
+            .Take(20)
+            .Select(i => new KeyValuePair<int, string>(i.FoodId, i.Name))
+            .ToListAsync();
+    }
+
+    public async Task<List<Ingredient>> GetAllAsync()
+    {
+        return await _context.Ingredients
+            .OrderBy(i => i.Name)
+            .ToListAsync();
+    }
+}
