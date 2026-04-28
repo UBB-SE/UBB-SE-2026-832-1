10f83bc6cc8f4e11605b5ba02b0c6be21d72c066
mirko2852 <mirko2852@icloud.com>
2026-04-28 17:39:50 +0300

    Unnecesary annotations removed

--- ClassLibrary/Models/Conversation.cs
+++ ClassLibrary/Models/Conversation.cs
@@ -1,16 +1,11 @@
 namespace ClassLibrary.Models;
 
-using System.ComponentModel.DataAnnotations;
-
 public class Conversation
 {
-    [Key]
     public int Id { get; set; }
 
-    [Required]
     public bool HasUnanswered { get; set; }
 
-    [Required]
     public virtual User User { get; set; } = default!;
 
     public virtual ICollection<Message> Messages { get; set; } = new List<Message>();
--- ClassLibrary/Models/Message.cs
+++ ClassLibrary/Models/Message.cs
@@ -1,23 +1,17 @@
 namespace ClassLibrary.Models;
 
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 public class Message
 {
-    [Key]
     public int Id { get; set; }
 
-    [Required]
     public DateTime SentAt { get; set; }
 
-    [Required]
     public virtual Conversation Conversation { get; set; } = default!;
 
-    [Required]
     public virtual User Sender { get; set; } = default!;
 
-    [Required]
     [MaxLength(5000)]
     public string TextContent { get; set; } = string.Empty;
 }
