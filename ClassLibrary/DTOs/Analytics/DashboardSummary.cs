dc5abf592dda2cd47146bc9555c6d3867d867b6e
Vlad Florin-Alexandru <vladfalex@gmail.com>
2026-04-28 22:37:23 +0300

    added analytics dtos

---
+++ ClassLibrary/DTOs/Analytics/DashboardSummary.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassLibrary.DTOs.Analytics;
+
+public sealed class DashboardSummary
+{
+    public int TotalWorkouts { get; init; }
+
+    public TimeSpan TotalActiveTimeLastSevenDays { get; init; }
+
+    public string? PreferredWorkoutName { get; init; }
+}
