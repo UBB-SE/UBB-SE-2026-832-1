d33ad2de292fa918fce0cf6a688496a138494002
AndaChiorean <andachiorean@users.noreply.github.com>
2026-04-29 19:35:00 +0300

    add FoodItem and MealPlan DTOs

---
+++ ClassLibrary/DTOs/FoodItemDto.cs
@@ -0,0 +1,30 @@
+namespace ClassLibrary.DTOs;
+
+public sealed class FoodItemDto
+{
+    public int FoodItemId { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public int Calories { get; set; }
+
+    public int Carbohydrates { get; set; }
+
+    public int Fat { get; set; }
+
+    public int Protein { get; set; }
+
+    public bool IsVegan { get; set; }
+
+    public bool IsKeto { get; set; }
+
+    public bool IsGlutenFree { get; set; }
+
+    public bool IsLactoseFree { get; set; }
+
+    public bool IsNutFree { get; set; }
+
+    public string Description { get; set; } = string.Empty;
+
+    public string ImageUrl { get; set; } = string.Empty;
+}
