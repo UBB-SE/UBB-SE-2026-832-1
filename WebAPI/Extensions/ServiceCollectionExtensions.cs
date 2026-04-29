60ce54464b96b06160141af3c5be91aed89f776f
AndaChiorean <andachiorean@users.noreply.github.com>
2026-04-29 20:15:00 +0300

    implement FoodItemService and controller

--- WebAPI/Extensions/ServiceCollectionExtensions.cs
+++ WebAPI/Extensions/ServiceCollectionExtensions.cs
@@ -8,6 +8,7 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddWebApiServices(this IServiceCollection services)
     {
         services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IFoodItemService, FoodItemService>();
         return services;
     }
 }
