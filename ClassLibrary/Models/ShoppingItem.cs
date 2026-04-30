d3947884230bf9f0288291f9fb0aa3ad6dc509d3
Baldean Stefan-Tudor <baldeanstefan420@gmail.com>
2026-04-30 14:53:50 +0300

    Added the ShoppingList service as well and migrated the ShoppingList model.

---
+++ ClassLibrary/Models/ShoppingItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Models
+{
+    public class ShoppingItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int IngredientId { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string IngredientName { get; set; } = string.Empty;
+
+        [Required]
+        public double QuantityGrams { get; set; }
+
+        [Required]
+        public bool IsChecked { get; set; }
+
+        [ForeignKey("UserId")]
+        public User? User { get; set; }
+
+        [ForeignKey("IngredientId")]
+        public Ingredient? Ingredient { get; set; }
+    }
+}
---
+++ WinUI/Services/IShoppingListService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary.DTOs;
+
+namespace WinUI.Services
+{
+    public interface IShoppingListService
+    {
+        Task<IEnumerable<ShoppingItemDto>> GetShoppingItemsAsync(int userId);
+        Task<ShoppingItemDto?> AddItemAsync(int userId, AddShoppingItemRequest request);
+        Task<bool> RemoveItemAsync(int itemId);
+        Task<bool> MoveToPantryAsync(int itemId);
+    }
+}
