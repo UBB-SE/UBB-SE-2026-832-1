301950597ac16ed11ca4ccd2fa109b09f6873c1c
Vlad Florin-Alexandru <vladfalex@gmail.com>
2026-05-03 19:02:13 +0300

    added daily log totals dto

---
+++ ClassLibrary/DTOs/DailyLogTotalsDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibrary.DTOs;
+
+public sealed class DailyLogTotalsDto
+{
+    public int TotalCalories { get; set; }
+
+    public int TotalProtein { get; set; }
+
+    public int TotalCarbohydrates { get; set; }
+
+    public int TotalFat { get; set; }
+}
