fcd17734158479536cb3c693ac64ca86ef135b42
Vlad Florin-Alexandru <vladfalex@gmail.com>
2026-05-05 14:29:31 +0300

    rem serv

---
+++ ClassLibrary/DTOs/ReminderDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ClassLibrary.DTOs
+{
+    public sealed class ReminderDto
+    {
+        //i added this class so the reminder service compiles, the reminder model is not yet implemented.
+    }
+}
