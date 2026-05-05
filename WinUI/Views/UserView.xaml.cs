5136830632ff84019e81a28b88ee6b2ff69b66f1
Alawerga <adrianfdez.rdez@gmail.com>
2026-05-05 03:17:43 +0300

    integrate DailyMealPlanView and UserView #208 #209

---
+++ WinUI/Views/UserView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using WinUI.ViewModels;
+
+namespace WinUI.Views;
+
+public sealed partial class UserView : Page
+{
+    public UserViewModel ViewModel { get; }
+
+    public UserView()
+    {
+        this.InitializeComponent();
+        this.ViewModel = new UserViewModel();
+    }
+}
