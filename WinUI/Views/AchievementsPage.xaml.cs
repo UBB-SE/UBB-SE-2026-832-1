5d161286bc0a2b694e2e11b2e0efa3687f24282a
AlexMarcus17 <alexmarcus5040717@gmail.com>
2026-05-08 20:16:39 +0300

    added achievements page

---
+++ WinUI/Views/AchievementsPage.xaml.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Net.Http;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using WinUI.Services;
+using WinUI.ViewModels;
+
+namespace WinUI.Views;
+
+public sealed partial class AchievementsPage : Page
+{
+    private readonly AchievementsViewModel viewModel;
+    private readonly UserSession userSession;
+
+    public AchievementsViewModel ViewModel => this.viewModel;
+
+    public AchievementsPage()
+    {
+        this.InitializeComponent();
+
+        this.userSession = new UserSession();
+        this.viewModel = new AchievementsViewModel(new AchievementsService(new HttpClient()));
+        this.DataContext = this.viewModel;
+
+        this.Loaded += this.Page_Loaded;
+    }
+
+    private async void Page_Loaded(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            await this.viewModel.LoadAchievementsCommand.ExecuteAsync(this.userSession.CurrentClientId).ConfigureAwait(true);
+        }
+        catch (Exception)
+        {
+            // Keep the page usable even if the service call fails.
+        }
+    }
+}
